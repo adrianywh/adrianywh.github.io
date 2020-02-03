4e76b3497b754635b544d656287cc5d1d9035486
Adrian <adrianywh@gmail.com>
2020-02-03 20:47:19 +0800

    added last updated timestamp for vgc19

--- js/vgc19teamsearch.js
+++ js/vgc19teamsearch.js
@@ -1,3 +1,4 @@
+var lastUpdated = "2019-October-20";
 var vgcTeams;
 
 $( document ).ready(function() {
@@ -66,6 +67,8 @@ $( document ).ready(function() {
     .then(function () {
         $("#vgcPlayer").select2();
     })
+
+    $("#lastUpdatedText").html(lastUpdated)
 })
 
 /********************vgc helper******************** */
